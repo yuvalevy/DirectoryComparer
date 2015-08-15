c43bb343f07b41eea4e76b06f4f39613e9cf6305
yuvalevy <yuval.yuki.levy@gmail.com>
2015-08-15 17:46:47 +0300

    * Fixed 'arrange lists' #4 \n*Added to 'Calc' button the abitily to add new files from directory #1

--- excelVBScript.vb
+++ excelVBScript.vb
@@ -1,15 +1,16 @@
 Public inDebugMode As Boolean
 
 Private Sub CalcButton_Click()
-    inDebugMode = True
+    inDebugMode = False
     Call EditingSheet(False)
     
     Dim directoryPath As String
     directoryPath = Range("B2")
      
-    Call CalcFilesByDirectory(directoryPath)
+    Call CalcFilesByDirectory
     
     Call FixButton(CalcButton, 87, 70)
+    Call DeleteEmptyRows
     Call EditingSheet(True)
 End Sub
 
@@ -25,93 +26,108 @@ End Sub
 
 Sub DeleteEmptyRows()
     
-    ' Dim cellsForDelete
     Set myCells = shtActive.UsedRange
     
     Dim rowCount As Long
     Dim colCount As Long
+    Dim isWholeRowEmpty As Boolean
     
     rowCount = myCells.Rows.Count
     colCount = myCells.Columns.Count
     
-    Call MessageBox("Row: " & rowCount & " Col: " & colCount)
+    Call MessageBox("Last Row: " & rowCount & "Last Col: " & colCount)
     
     ReDim myArray(rowCount, colCount) As Object
     
     
     ' ## check the bowndries
     ' If cell2.Row > 3 And (cell2.Column = 3 Or cell2.Column = 4) Then
-    
-    
-    Dim isWholeRowEmpty As Boolean
-    
-    For RowIndex = 4 To rowCount
-         wholeRowEmpty = True
+      
+    For rwIndex = rowCount To 4 Step -1
+        wholeRowEmpty = True
        
-        For colIndex = 2 To 3
-            Call MessageBox(myCells(RowIndex, colIndex).Value)
+        For clIndex = 2 To 3
+            Call MessageBox(myCells(rwIndex, clIndex).Value)
             
-            If myCells(RowIndex, colIndex) <> "" Then
-            
-                Call MessageBox(colIndex & "<>")
-                 wholeRowEmpty = False
-            
+            If myCells(rwIndex, clIndex) <> "" Then
+                Call MessageBox(clIndex & "<>")
+                wholeRowEmpty = False
             End If
+        
         Next
         
         If wholeRowEmpty = True Then
-            Call MessageBox("Deleting row  " & RowIndex)
-            myCells(RowIndex, 0).EntireRow.Delete
+            Call MessageBox("Deleting row  " & rwIndex)
+            myCells(rwIndex, 0).EntireRow.Delete
         End If
-    Next
     
-    Call EditingSheet(True)
+    Next
 
 End Sub
 
-Sub CalcFilesByDirectory(directory As String)
+Sub CalcFilesByDirectory()
     
+    Dim lastWritenInRow As String
+    Dim srcStartCell As String
+    Dim desStartCell As String
+    Dim srcEndCell As String
+    Dim srcLst As String
+    Dim srcRangeValue As String
+    Dim fileName As String
     Dim cellLetter As String
+    Dim directoryCell As String
+    Dim directoryPath As String
+    Dim directoryPattern As String
+    Dim srcRangeLst As Range
+    
+    lastWritenInRow = shtActive.UsedRange.Rows.Count
     
     ' ##Get Configuration
-    Set directoryPattern = shtConfig.Cells(3, 2).Value
-    Set srcStartCell = shtConfig.Cells(4, 2).Value
-    Set desStartCell = shtConfig.Cells(5, 2).Value
-    Set lastWritenInRow = shtConfig.Cells(1, 2).Value
+    directoryCell = shtConfig.Cells(1, 2).Value 'source directory cell
+    cellLetter = shtConfig.Cells(2, 2).Value 'source cell letter
+    directoryPattern = shtConfig.Cells(3, 2).Value 'directory pattern
+    srcStartCell = shtConfig.Cells(4, 2).Value 'srcStartCell
+    desStartCell = shtConfig.Cells(5, 2).Value 'desStartCell
+    srcEndCell = cellLetter & lastWritenInRow
+    srcRangeValue = srcStartCell & ":" & srcEndCell
+    
+    ' ##Get directory path from sheet
+     
+    directoryPath = Range(directoryCell).Value
     
     ' ##Checks if last char in directory is '\'
     If Right(directoryPath, 1) <> "\" Then
         directoryPath = directoryPath & "\"
     End If
+     
+    fileName = Dir(directoryPath & "*")
     
-    
-    Set srcLst = Range()
-    
-   ' fileName = Dir(directoryPath & directoryPattern)
-    
-   ' cellLetter = "B"
-   ' Index = 1
-   ' Do While fileName <> ""
-   '     Call MessageBox(fileName)
-   '     Range(cellLetter & Index).Value = fileName
-   '     Index = Index + 1
-   '     fileName = Dir()
-   ' Loop
-    
-   ' cellLetter = "C"
-   ' fileName = Dir(directory & directoryPattern)
-      
-   'Range(cellLetter & Index).Value (fileName)
-      
+
+    Dim i As Integer
+    Dim foundVal As String
+
+    i = 1
+    Do While fileName <> ""
+        Call MessageBox(fileName)
+        Set vals = Range(srcRangeValue).Find(fileName)
         
+        If (vals Is Nothing) Then
+            ' ##Adds new file to the last row
+            lastWritenInRow = lastWritenInRow + 1
+            Range(cellLetter & lastWritenInRow).Value = fileName
+        End If
+        i = i + 1
+        fileName = Dir()
+    Loop
+
+    'fileName = Dir(directoryPath & directoryPattern)
+      
+'   Range(cellLetter & Index).Value (fileName)
     
-    
-    MsgBox "Finish"
+    MessageBox "Finish CalcFilesByDirectory"
     
 
 End Sub
-
-
 ' ### HELP SUBs
 
 Private Sub MessageBox(messageToDesplay As String)
@@ -134,3 +150,4 @@ Private Sub FixButton(button As MSForms.CommandButton, top As Integer, left As I
     button.top = top
     button.left = left
 End Sub
+
